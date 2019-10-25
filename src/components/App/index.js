6ac423af8320504f216843066d2ef55ad189d2aa
GolovaVasilenko <vasilenko75@gmail.com>
2019-10-25 21:31:10 +0300

    ok

---
+++ src/components/App/index.js
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+import Footer from '../static/Footer';
+import Header from '../static/Header';
+import Home from '../pages/Home'
+
+
+function App() {
+  
+  const [value, setValue] = useState('');
+  
+  const changeInput = (event) => {
+    //const val = event.target.value
+    //setValue(val);
+    setValue(event.target.value);
+  }
+  return (
+    <div id='app'>
+      <Header />
+      <main> 
+        <input onChange={changeInput}/>
+        <Home
+       // title = 'my page home'
+        title ={value}
+        numberPage = '5'
+        />
+        <p>{value}</p>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
